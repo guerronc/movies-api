b0a0c0855b9d78b8eefd60e67f65e4b29ed1017c
Cesar Guerron <guerronc@outlook.com>
2019-11-17 18:51:37 -0500

    Agregar test para las rotas de la API

--- test/routes.movies.test.js
+++ test/routes.movies.test.js
@@ -1,18 +1,29 @@
 const assert = require('assert');
 const proxyquire = require('proxyquire');
 
-const {MoviesServiceMocks,moviesMock} = require('../utils/mocks/movies')
+const { MoviesServiceMocks, moviesMock } = require('../utils/mocks/movies')
 
 const testServer = require('../utils/testServer');
 
-describe('routes-movies', function () {
-    const route = proxyquire('../routes/movies.js',{
-        '../services/movies.js': MoviesServiceMocks
+describe('routes - movies', function () {
+    const route = proxyquire('../routes/movies', {
+        '../services/movies': MoviesServiceMocks
     })
-})
+    const request = testServer(route);
 
-const request = testServer(route);
+    describe('GET /movies', function () {
+        it('Should respond with status 200', function (done) {
+            request.get('/api/movies').expect(200, done);
+        });
 
-describe('GET /movies', function () {
-    if()
+        it('Should respond with the list of movies', function (done) {
+            request.get('/api/movies').end((err,res)=>{
+                assert.deepEqual(res.body, {
+                    data: moviesMock,
+                    message: 'Movies listed'
+                });
+                done();
+            })
+        })
+    })
 })
