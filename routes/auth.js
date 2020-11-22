eda4994a386f34b0aa9a3f5239421da0212b95e8
Cesar Guerron <guerronc@outlook.com>
2020-11-22 17:36:06 -0500

    Create endpoint for authetication with others

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,10 @@ const UsersService = require('../services/users');
 const validationHandler = require('../utils/middleware/validationHandler');
 
 const { config } = require('../config');
-const { createUserSchema } = require('../utils/schemas/users');
+const {
+  createUserSchema,
+  createProviderUserSchema
+} = require('../utils/schemas/users');
 
 require('../utils/auth/strategies/basic');
 
@@ -64,13 +67,50 @@ const authApi = app => {
     validationHandler(createUserSchema),
     async (req, res, next) => {
       const { body: user } = req;
-      
+
       try {
         const createUserId = await userService.createUser({ user });
         res.status(201).json({
           data: createUserId,
           message: 'User created'
         });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+  router.post(
+    '/sign-provider',
+    validationHandler(createProviderUserSchema),
+    async (req, res, next) => {
+      const { body } = req;
+      const { apiKeyToken, ...user } = body;
+      if (!apiKeyToken) {
+        next(boom.unauthorized('apiKeyToken is required'));
+      }
+
+      try {
+        const queriedUser = await userService.getOrCreateUser({ user });
+        const apiKey = await apiKeyService.getApiKey({ token: apiKeyToken });
+
+        if (!apiKey) {
+          next(boom.unauthorized());
+        }
+
+        const { _id: id, name, email } = queriedUser;
+        const payload = {
+          sub: id,
+          name,
+          email,
+          scopes: apiKey.scopes
+        };
+
+        const token = jwt.sign(payload, config.authJwtSecret, {
+          expiresIn: '15m'
+        });
+
+        return res.status(200).json({ token, user: { id, name, email } });
       } catch (error) {
         next(error);
       }
--- services/users.js
+++ services/users.js
@@ -12,17 +12,23 @@ class UsersService {
   }
 
   async createUser({ user }) {
-    const {name, email, password} = user;
+    const { name, email, password } = user;
     const hashPassword = await bcrypt.hash(password, 10);
     const createUserId = await this.mongoDB.create(this.collection, {
       name,
       email,
       password: hashPassword
     });
-
-    
-
     return createUserId;
+  }
+
+  async getOrCreateUser({ user }) {
+    const queriedUser = await this.getUser({ email: user.email });
+    if (queriedUser) {
+      return queriedUser;
+    }
+    await this.createUser({ user });
+    return await this.getUser({ email: user.email });
   }
 }
 
